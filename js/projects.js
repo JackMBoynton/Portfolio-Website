ba95e5cc2ab67623261c9d528ed051f97ff77906
Jack Boynton <boyntonj.dev@gmail.com>
2020-02-02 14:36:33 +0000

    Finished home and projects, cleaner design now.

--- js/projects.js
+++ js/projects.js
@@ -1,47 +1,49 @@
 jQuery.githubUser = function(username, callback) {
   jQuery.getJSON(
-    "https://api.github.com/users/" + username + "/repos?callback=?",
+    "https://api.github.com/users/" + username + "/repos?callback=?", // Function that retrieves the repos.
     callback
   );
 };
 
 jQuery.fn.loadRepositories = function(username) {
   this.html(
-    "<span>Querying GitHub for " + username + "'s repositories...</span>"
+    "<span>Pulling projects from my GitHub (" + username + ") repositories...</span>" // Placeholder for when we are retrieving the repos from GitHub.
   );
 
-  var target = this;
+  var target = this; // Making the target the element where this function is being called from, in this case div id=githubProjects from projects.html
   $.githubUser(username, function(data) {
     var repos = data.data; // JSON Parsing
     sortByName(repos);
 
-    var table = $("<table>");
+    var table = $("<table>"); // Creating a empty table as a variable.
     target.empty().append(table);
     table.append(
-      '<tr><th>Project Name</th><th class="right-side">Project Description</th></tr>'
+      '<tr><th>Project Name</th><th class="splitter-side">Project Description</th><th class="splitter-side">Source Code</th></tr>'// Appending the table headings to the table.
     );
-    $(repos).each(function() {
+    $(repos).each(function() { // for each repo, run the following code on them.
       if (this.name != username.toLowerCase() + ".github.com") {
-        table.append(
-          '<tr><td><a target="_blank" href="' +
-          (this.homepage ? this.homepage : this.html_url) + // href link
-          '">' +
+        table.append( 
+          '<tr><td>' +// Append a new row with a cell for the name and language together.
           this.name + // repo title.
-          "</a><em>" +
-          (this.language ? " (" + this.language + ")" : "") + // reoo language
-            "</em></td>" +
-            '<td class="right-side">' +
-            this.description +
-            "</td></tr>"
-        );
+          "<em>" +
+          (this.language ? " (" + this.language + ")" : "") + // repo language
+            "</em></td>" + // End that cell here
+            '<td class="splitter-side">' + // Splitter-side class is just so I can add border left easier.
+            this.description + // Create a new cell for the description of the current repo.
+            "</td>" + // Cell end
+            '<td class="splitter-side">' + // Again, new cell in the same row.
+            '<a target="_blank" href="' + // Create a link that will open in a new tab.
+            (this.homepage ? this.homepage : this.html_url) + // href link, project github page.
+            '">View <i class="fas fa-link"></i></a></td></tr>' // Add the a element label which in this case is 'View' with a font awesome link symbol.
+        ); // Therefore, this is simply parsing the JSON data we pull in into table cells for each repo, each repo being a row in the table with 3 cells for name, desc and source code.
       }
-      table.append("</table>");
+      table.append("</table>"); // Finally, finish the table tag so it becomes an actual table.
     });
   });
 
   function sortByName(repos) {
     repos.sort(function(a, b) {
-      return a.name - b.name;
+      return a.name - b.name; // Sort the repos in alphabetical order.
     });
   }
 };
